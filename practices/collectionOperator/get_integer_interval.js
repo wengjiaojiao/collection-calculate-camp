8c64169a2692c96743971ab4f648cfb8c5fb1ed8
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-25 14:52:07 +0800

    finish the third practice

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+    var result = [];
+    if(number_a <= number_b) {
+        for(var i=number_a; i<=number_b; i++) {
+            result.push(i);
+        }
+    }else {
+        for(var j=number_a; j>=number_b; j--) {
+            result.push(j);
+        }
+    }
+    return result;
 }
 
 module.exports = get_integer_interval;
-
