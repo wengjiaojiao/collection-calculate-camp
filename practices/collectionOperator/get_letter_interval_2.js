ebf18d136008917685f7c6279633143a3cc09fc8
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-25 16:36:11 +0800

    seventh version 1

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,8 +1,29 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+    var result = [];
+    var judge = false;
+    if(number_a > number_b) {
+        var c = number_b;
+        number_b = number_a;
+        number_a = c;
+        judge = true;
+    }
+    for(var i=number_a;i<=number_b;i++) {
+        if(i<=26) {
+            result.push(String.fromCharCode(i+96));
+        }else if(26<i && i<53) {
+            var j = i - 26;
+            result.push("a"+String.fromCharCode(j+96));
+        }else {
+            var k = i -52;
+            result.push("b"+String.fromCharCode(k+96));
+        }
+    }
+    if(judge) {
+        result.reverse();
+    }
+    return result;
 }
 
 module.exports = get_letter_interval_2;
-
