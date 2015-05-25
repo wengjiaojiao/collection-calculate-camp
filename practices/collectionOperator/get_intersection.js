1edb0a2e4e31737a5a21a042d2325c05f232966e
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-25 14:54:50 +0800

    simplify method

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,11 +2,8 @@
 
 function get_intersection(collection_a, collection_b) {
     var _ = require('../lodash/array');
-    var same = _.intersection(collection_a,collection_b);
-    var last = same.pop();
-
-    same.unshift(last);
-    return same;
+    
+    return  _.intersection(collection_b,collection_a);
 }
 
 module.exports = get_intersection;
