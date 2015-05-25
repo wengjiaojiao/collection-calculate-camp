33bc566bd12694c046534da741fe1cd19feb7534
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-25 15:16:13 +0800

    finish the forth practice

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,23 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+    var _ = require('../lodash/array');
+    var collection = [];
+
+    if(number_a <= number_b) {
+        for(var i=number_a; i<=number_b; i++) {
+            collection.push(i);
+        }
+    }else {
+        for(var j=number_a; j>=number_b; j--) {
+            collection.push(j);
+        }
+    }
+    var result = _.remove(collection,function(n){
+        return n % 2 == 0;
+        }
+    )
+    return result;
 }
 
 module.exports = get_integer_interval_2;
