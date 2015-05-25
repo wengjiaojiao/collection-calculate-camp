3a1dc672d595997675dcc40ac102deaa0c9a2a47
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-25 17:15:32 +0800

    finish filter first practice

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_even(collection) {
+    var _ = require('../lodash/array');
+    var evens = _.remove(collection, function(n) {
+    return n % 2 == 0;
+  });
 
-  //在这里写入代码
+    return evens;
 }
 
 module.exports = choose_even;
