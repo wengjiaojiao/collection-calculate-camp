8757ce857a8adffefcc4acce66784e63fbc7f98d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-25 15:05:03 +0800

    finish the eigth practice

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+    var _ = require('../lodash/array');
+
+    return _.union(collection_a,collection_b);
 }
 
 module.exports = get_union;
-
