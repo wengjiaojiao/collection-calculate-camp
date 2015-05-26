9e4f0a52044317986805eb65829990924691519c
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 17:30:14 +0800

    run again without lodash

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,9 +1,30 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-    var _ = require('../lodash/array');
-    
-    return  _.intersection(collection_b,collection_a);
+    var collection = [];
+    for(var i=0; i<collection_a.length; i++) {
+        for(var j=0; j< collection_b.length; j++) {
+            if(collection_a[i] === collection_b[j]) {
+                collection.push(collection_a[i]);
+            }
+        }
+    }
+    var last = collection.pop();
+    collection.unshift(last);
+    return collection;
 }
 
 module.exports = get_intersection;
+
+
+
+
+
+
+
+
+
+
+
+//var _ = require('../lodash/array');
+//return  _.intersection(collection_b,collection_a);
