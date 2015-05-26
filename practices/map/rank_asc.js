f1ff217fd21a4899d5b9dc49958974f9709f8a54
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 14:53:00 +0800

    sort and reverse the array

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,6 +1,9 @@
 'use strict';
 var rank_asc = function(collection){
-  return [6,5,4,3,2];
+    var _ = require("../lodash/collection");
+    var result= _.sortBy(collection);
+    
+    return result.reverse();
 };
 
 module.exports = rank_asc;
