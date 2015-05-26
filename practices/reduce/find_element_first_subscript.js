d3a70685837c76de672efdf2dd293c2bb6fabb6c
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 13:45:04 +0800

    run the first element

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  //在这里写入代码
+    var _  =require("../lodash/array");
+    return _.indexOf(collection,element);
 }
 
 module.exports = calculate_elements_sum;
-
