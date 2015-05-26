dc1204acd25cf14dbb146879ed1adda09b3a8790
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 10:56:16 +0800

    rn the second of map

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,10 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+  var _ = require('../lodash/collection');
+  
+  return _.map(collection,transform);
 };
-
+function transform(n) {
+    return (4 * n + 1);
+}
 module.exports = map_to_four_multiples_add_one;
