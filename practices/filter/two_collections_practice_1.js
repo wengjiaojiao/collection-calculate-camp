836d944857be1aa5d61648ba20ae7d70a06e6a32
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 18:28:28 +0800

    search the same of collections without lodash

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,9 +1,22 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-    var _ = require('../lodash/array');
+    var same = [];
 
-    return _.intersection(collection_a,collection_b);
+    for(var i=0; i<collection_a.length; i++) {
+        for(var j=0; j<collection_b.length; j++) {
+            if(collection_a[i] === collection_b[j]) {
+                same.push(collection_a[i]);
+            }
+        }
+    }
+    return same;
 }
 
 module.exports = choose_common_elements;
+
+
+
+
+
+//return _.intersection(collection_a,collection_b);
