53ba38009f8c2c9695ae2864a3c3802308599610
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 09:16:18 +0800

    filter the third practice finish

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
+    var _ = require('../lodash/array');
 
-  //在这里写入代码
+    return _.uniq(collection,true);
 }
 
 module.exports = choose_no_repeat_number;
