2c823a0e5f019b3d0721d5886b88d741f8363b13
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 11:32:55 +0800

    run the countby practice of filter

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -2,7 +2,11 @@
 
 function grouping_count(collection) {
 
-  //在这里写入代码
+    var _ = require("../lodash/collection");
+
+    return _.countBy(collection,function(n) {
+        return Math.floor(n);
+    })
 }
 
 module.exports = grouping_count;
