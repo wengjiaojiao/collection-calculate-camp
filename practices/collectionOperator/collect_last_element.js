6b5dd8e26aac646565ea0e35646a8f56d6ef75d3
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 17:16:59 +0800

    without lodash

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,9 +1,25 @@
 'use strict';
 
 function collect_last_element(collection) {
-    var _ = require('../lodash/array');
-
-    return _.last(collection);
+    var i = collection.length -1;
+    var last =collection[i];
+    return last;
 }
 
 module.exports = collect_last_element;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var _ = require('../lodash/array');
+
+//return _.last(collection);
