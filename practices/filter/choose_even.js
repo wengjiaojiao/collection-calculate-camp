c3a92a190e748f39288d3c4f8e166dc16e03bbf9
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 17:38:25 +0800

    find even without lodash

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,12 +1,36 @@
 'use strict';
 
 function choose_even(collection) {
-    var _ = require('../lodash/array');
-    var evens = _.remove(collection, function(n) {
-    return n % 2 == 0;
-  });
+    var even = [];
 
-    return evens;
+    for(var i =0; i<collection.length; i++) {
+        if(collection[i] % 2 == 0) {
+            even.push(collection[i]);
+        }
+    }
+    return even;
 }
 
 module.exports = choose_even;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var evens = _.remove(collection, function(n) {
+return n % 2 == 0;
+});
+
+return evens;
+*/
