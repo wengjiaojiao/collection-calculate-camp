76c532221674fad34227614861955010a8a4f927
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 13:49:06 +0800

    run the last element

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  //在这里写入代码
+    var _ = require("../lodash/array");
+    
+    return _.lastIndexOf(collection,element);
 }
 
 module.exports = calculate_elements_sum;
