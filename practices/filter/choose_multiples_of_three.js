d31ffea0df68bad2ba9c1d5344b37728ff49678e
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 17:41:14 +0800

    calculate threetimes without lodash

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,12 +1,27 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-    var _ = require('../lodash/array');
+    var time = [];
 
-    return _.remove(collection,function(n){
-        return n % 3 == 0;
-    })
-
+    for(var i=0; i<collection.length; i++) {
+        if(collection[i] % 3 == 0) {
+            time.push(collection[i]);
+        }
+    }
+    return time;
 }
 
 module.exports = choose_multiples_of_three;
+
+
+
+
+
+
+
+
+
+/*return _.remove(collection,function(n){
+    return n % 3 == 0;
+});
+*/
