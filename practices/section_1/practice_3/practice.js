53cc4eb9f46b8872c5908c4899fdbb1a5d91bb90
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 15:25:46 +0800

    intersect value

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,7 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+    var _ = require("../../lodash");
+
+    return _.intersection(collection_a,object_b.value);
 }
 
 module.exports = collect_same_elements;
