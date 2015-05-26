3684a00006d46f240d90aa3796bab3e5ecd17647
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 14:57:26 +0800

    calculate the average

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+    var _ = require("../lodash/math");
+    var sum = _.sum(collection);
+    return sum/collection.length; 
 }
 
 module.exports = compute_average;
-
