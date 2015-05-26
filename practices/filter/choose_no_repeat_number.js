3d063f13b7c49d07a571a6f07518fedd70b68136
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 18:19:11 +0800

    no repeat without lodash

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,9 +1,29 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-    var _ = require('../lodash/array');
+    var no_repeat = [];
 
-    return _.uniq(collection,true);
+    for(var i=0; i<collection.length; i++) {
+        var exist = false;
+
+        for(var j=0; j<no_repeat.length; j++) {
+            if(collection[i] === no_repeat[j]) {
+                exist = true;
+            }
+        }
+        if(!exist) {
+        no_repeat.push(collection[i]);
+        }
+    }
+    return no_repeat;
 }
 
 module.exports = choose_no_repeat_number;
+
+
+
+
+
+
+
+//return _.uniq(collection,true);
