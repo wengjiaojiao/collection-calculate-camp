9ea9e061129c1968f3fb2b9f32448e33ea39d3ef
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 10:49:16 +0800

    run the first practice of map

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,9 @@
 'use strict';
 function map_to_even(collection){
-  return [2,4,6,8,10];
+    var _ = require('../lodash/collection');
+    return _.map(collection,twotimes);
+}
+function twotimes(n) {
+    return n * 2;
 }
 module.exports = map_to_even;
