f7861dee6c17a5c975019da12e9b5a853e36a515
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 13:59:19 +0800

    run the first even

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function find_first_even(collection) {
-  //在这里写入代码
+    var _ =require("../lodash/collection");
+
+    return _.find(collection,function(n) {
+        return n % 2 ==0;
+    })
 }
 
 module.exports = find_first_even;
-
