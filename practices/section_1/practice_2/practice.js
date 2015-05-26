4e29ca9f980c7f6f8a0a238a8ea3745aa34badcb
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 15:11:09 +0800

    flatten and intersection

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,8 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+    var _ = require("../../lodash");
+
+    collection_b = _.flatten(collection_b);
+    return _.intersection(collection_a,collection_b);
 }
 
 module.exports = collect_same_elements;
