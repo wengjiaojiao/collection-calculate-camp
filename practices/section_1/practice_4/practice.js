cfb1e03b1705adea1ab4679345a8aac045112e41
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 15:47:11 +0800

    value and key

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,9 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+    var _ = require("../../lodash");
+    for(var i=0; i<collection_a.length; i++) {
+        collection_a[i] = collection_a[i].key;
+    }
+    return _.intersection(collection_a,object_b.value);
 }
 
 module.exports = collect_same_elements;
