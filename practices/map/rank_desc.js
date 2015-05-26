c00a10adb236404e29d598c82123a96d60df1023
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 11:38:28 +0800

    run the asending practice

--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,8 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
-};
+    var _ = require("../lodash/collection");
+
+    return _.sortBy(collection);
+  };
 
 module.exports = rank_desc;
