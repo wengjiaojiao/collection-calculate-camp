941ea5532394d0be9d725b45388dde941e31feb8
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-26 11:05:43 +0800

    run the third of map

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,6 +1,10 @@
 'use strict';
 var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
-};
+    var _ = require('../lodash/collection');
 
+    return _.map(collections,threetimes);
+};
+function threetimes(n) {
+    return n * 3;
+}
 module.exports = map_to_three_multiples;
