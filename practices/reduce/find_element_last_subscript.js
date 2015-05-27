67ed254fb7d5b701c9f361f841f3ba0a40e4dce8
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 15:46:53 +0800

    find last without lodash

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -1,9 +1,17 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-    var _ = require("../lodash/array");
-    
-    return _.lastIndexOf(collection,element);
+    for (var i = collection.length-1; i >= 0; i--) {
+        if(collection[i] === element) {
+            return i;
+            break;
+        }
+    }
+
 }
 
 module.exports = calculate_elements_sum;
+
+
+
+//return _.lastIndexOf(collection,element);
