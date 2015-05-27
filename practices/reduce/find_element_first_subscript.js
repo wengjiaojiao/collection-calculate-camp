f7c69668828a35a4fef9cecc4942c4dc51ed3bad
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 16:49:31 +0800

    delete break

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -4,7 +4,6 @@ function calculate_elements_sum(collection, element) {
     for (var i = 0; i < collection.length; i++) {
         if(collection[i] === element) {
             return i;
-            break;
         }
     }
 }
