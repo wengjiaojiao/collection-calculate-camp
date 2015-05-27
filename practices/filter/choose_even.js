321e088b4cb0bd8debbd4bd8347ad91dd4cbf3a4
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 09:06:49 +0800

    aliging for form

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,7 +3,7 @@
 function choose_even(collection) {
     var even = [];
 
-    for(var i =0; i<collection.length; i++) {
+    for(var i = 0; i < collection.length; i++) {
         if(collection[i] % 2 == 0) {
             even.push(collection[i]);
         }
