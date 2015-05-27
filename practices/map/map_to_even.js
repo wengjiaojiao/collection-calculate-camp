5dc6b93c0f2250cd51b420b5e3e161475c710539
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 12:21:40 +0800

    map without lodash

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,9 +1,21 @@
 'use strict';
 function map_to_even(collection){
-    var _ = require('../lodash/collection');
-    return _.map(collection,twotimes);
+    var map = [];
+    for(var i = 0; i < collection.length; i++) {
+        collection[i] = collection[i] * 2;
+        map.push(collection[i]);
+    }
+    return map;
 }
-function twotimes(n) {
-    return n * 2;
-}
+
 module.exports = map_to_even;
+
+
+
+
+/*return _.map(collection,twotimes);
+
+function twotimes(n) {
+return n * 2;
+}
+*/
