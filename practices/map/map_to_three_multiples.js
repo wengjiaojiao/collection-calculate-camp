b534382a402074e59d065f293aa0318903ead6fe
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 12:24:32 +0800

    map threetimes without lodash

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,10 +1,10 @@
 'use strict';
 var map_to_three_multiples = function(collections){
-    var _ = require('../lodash/collection');
-
-    return _.map(collections,threetimes);
-};
-function threetimes(n) {
-    return n * 3;
+    var map = [];
+    for(var i = 0; i < collections.length; i++) {
+        collections[i] = collections[i] * 3;
+        map.push(collections[i]);
+    }
+    return map;
 }
 module.exports = map_to_three_multiples;
