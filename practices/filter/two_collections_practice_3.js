c9a7ca94b9d4e39bf777223195f690fb0673a1be
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 09:52:19 +0800

    aligning form

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -2,6 +2,7 @@
 
 function choose_divisible_integer(collection_a, collection_b) {
     var result = [];
+
     for(var i = 0; i < collection_a.length; i++) {
         for(var j = 0; j < collection_b.length; j++) {
             if(collection_a[i] % collection_b[j] == 0) {
