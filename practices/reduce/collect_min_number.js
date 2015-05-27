49e139c9a9535c1a8a8195470d84012bd4d5cb7e
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 15:05:19 +0800

    find min without lodash

--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -1,9 +1,14 @@
 'use strict';
 
 function collect_min_number(collection) {
-    var _ = require('../lodash/collection');
+    var min = 0;
 
-    return _.min(collection);
+    for (var i = 0; i < collection.length; i++) {
+        if(collection[min] > collection[i]) {
+            min = i;
+        }
+    }
+    return collection[min];
 }
 
 module.exports = collect_min_number;
