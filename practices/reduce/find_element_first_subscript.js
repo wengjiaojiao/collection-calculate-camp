4204e43a3d59b3da160f2be39ed921de4b6dcb52
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 15:43:51 +0800

    find the first without lodash

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -1,9 +1,16 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-    var _  =require("../lodash/array");
-
-    return _.indexOf(collection,element);
+    for (var i = 0; i < collection.length; i++) {
+        if(collection[i] === element) {
+            return i;
+            break;
+        }
+    }
 }
 
 module.exports = calculate_elements_sum;
+
+
+
+//return _.indexOf(collection,element);
