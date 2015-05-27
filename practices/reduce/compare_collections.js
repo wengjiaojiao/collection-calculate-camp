9ab5cd482dbc15e98b4c3ed6a8fd7bc986bd2301
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 15:13:03 +0800

    issame without lodash

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-    var _ = require('../lodash/lang');
-    return _.isEqual(collection_a,collection_b);
+    for (var i = 0; i < collection_a.length; i++) {
+        if(collection_a.length != collection_b.length) {
+            break;
+        }
+        if(collection_a[i] === collection_b[i]) {
+            return true;
+        }
+    }
 }
-
 module.exports = compare_collections;
