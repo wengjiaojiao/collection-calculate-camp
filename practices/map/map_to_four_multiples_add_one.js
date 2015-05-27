97b59fcd65ba0b4e535a60d24502727d3936c4fc
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 12:22:59 +0800

    map four add one without lodash

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,10 +1,10 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  var _ = require('../lodash/collection');
-  
-  return _.map(collection,transform);
-};
-function transform(n) {
-    return (4 * n + 1);
+    var map = [];
+    for(var i = 0; i < collection.length; i++) {
+        collection[i] = collection[i] * 4 + 1;
+        map.push(collection[i]);
+    }
+    return map;
 }
 module.exports = map_to_four_multiples_add_one;
