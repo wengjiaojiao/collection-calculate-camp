081376aa1a8c8eb6a10df309e919861092f14db6
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-27 10:36:50 +0800

    find difference without lodash

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,9 +1,19 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-    var _ = require('../lodash/array');
-
-    return _.difference(collection_a,collection_b);
+    for(var i = 0; i < collection_a.length; i++) {
+        for(var j = 0; j < collection_b.length; j++) {
+            if(collection_a[i] === collection_b[j]) {
+                collection_a.splice(i,1);
+                collection_b.splice(j,1);
+            }
+        }
+    }
+    var difference = collection_a.concat(collection_b);
+    return difference;
 }
 
 module.exports = choose_no_common_elements;
+
+
+//return _.difference(collection_a,collection_b);
