6d6c3dcbe66c243d8b848a65b78cfdbbbcae4656
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 16:26:42 +0800

    find the median

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -13,9 +13,9 @@ function compute_median(collection) {
         }
     }
 
-    var middle = parseInt(collection.length / 2);
     var median;
-
+    var middle = parseInt(collection.length / 2);
+    
     if(collection.length % 2 == 0 ) {
         median = (collection[middle] + collection[middle-1]) / 2;
     }else {
