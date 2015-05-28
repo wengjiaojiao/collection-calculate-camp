175129e8204ebb58df0887edc8986504481fd189
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 13:03:16 +0800

    change the form

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,6 @@
 function collect_same_elements(collection_a, object_b) {
     var same = [] ;
+    
     for (var i = 0; i < collection_a.length; i++) {
         for (var j = 0; j < object_b.value.length; j++) {
             if(collection_a[i] === object_b.value[j]) {
