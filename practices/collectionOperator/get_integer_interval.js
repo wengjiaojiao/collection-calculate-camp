cb86389ab45edc7c79b2278168185b495d34d8ec
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 18:36:28 +0800

    format adjusting

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,6 +2,7 @@
 
 function get_integer_interval(number_a, number_b) {
     var result = [];
+
     if(number_a <= number_b) {
         for(var i=number_a; i<=number_b; i++) {
             result.push(i);
