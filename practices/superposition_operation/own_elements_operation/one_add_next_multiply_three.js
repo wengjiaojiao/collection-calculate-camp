d6015c180c49376ad440a667a92463e04dfc102b
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 18:32:58 +0800

    format adjusting

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,6 +1,7 @@
 'use strict';
 function one_add_next_multiply_three(collection){
     var temp = [];
+    
     for (var i = 0; i < collection.length; i++) {
         if(i < collection.length -1) {
             temp[i] = (collection[i] + collection[i+1]) * 3;
