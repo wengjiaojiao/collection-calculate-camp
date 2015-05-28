750382891dd57f591e8cbd68ec723476f19c9c50
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 20:37:27 +0800

    remove break

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -4,7 +4,6 @@ function calculate_elements_sum(collection, element) {
     for (var i = collection.length-1; i >= 0; i--) {
         if(collection[i] === element) {
             return i;
-            break;
         }
     }
 
