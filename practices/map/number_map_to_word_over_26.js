0f7ae187ec472ab77075bf2f374d2e95eb2f6adc
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 16:23:02 +0800

    map to word over 26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -7,7 +7,7 @@ var number_map_to_word_over_26 = function(collection){
         var x =collection[i];
         var letter = [] ;
 
-        x = collection[i]/SIMPLE > 1 ? 1 : x
+        x = collection[i]/SIMPLE > 1 ? parseInt(collection[i]/SIMPLE) : x
         if(collection[i] > SIMPLE) {
             collection[i] -= SIMPLE;
             letter = String.fromCharCode(x+96);
