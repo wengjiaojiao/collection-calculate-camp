7259f85bece4e6cc5575ae15bf8530c592417ffc
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 21:19:22 +0800

    the even numbers' aver

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,17 @@
 'use strict';
 var calculate_average = function(collection){
+    var even = [];
 
+    for (var i = 0; i < collection.length; i++) {
+        if((i+1) % 2 == 0) {
+            even[even.length] = collection[i];
+        }
+    }
+    var sum = 0;
+
+    for (var j = 0; j < even.length; j++) {
+        sum += even[j];
+    }
+    return sum/even.length;
 };
 module.exports = calculate_average;
