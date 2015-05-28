cbfa8682d027479197694df57469274c2ddbb561
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 17:05:58 +0800

    calculate and sum

--- practices/superposition_operation/collection_operation/practice_1.js
+++ practices/superposition_operation/collection_operation/practice_1.js
@@ -1,9 +1,17 @@
 'use strict';
 
 function hybrid_operation(collection) {
+    var temp = [];
 
-  //在这里写入代码
+    for (var i = 0; i < collection.length; i++) {
+            temp.push(collection[i] * 3 + 2);
+    }
+    var sum = 0;
+
+    for (var j = 0; j < temp.length; j++) {
+        sum += temp[j];
+    }
+    return sum;
 }
 
 module.exports = hybrid_operation;
-
