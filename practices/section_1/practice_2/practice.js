2dd288444a6eebe049b1509fdb7c3c3070251553
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 13:01:04 +0800

    find the same elements without lodash

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,8 +1,13 @@
 function collect_same_elements(collection_a, collection_b) {
-    var _ = require("../../lodash");
-
-    collection_b = _.flatten(collection_b);
-    return _.intersection(collection_a,collection_b);
+    var same = [] ;
+    for (var i = 0; i < collection_a.length; i++) {
+        for (var j = 0; j < collection_b[0].length; j++) {
+            if(collection_a[i] === collection_b[0][j]) {
+                same.push(collection_a[i]);
+            }
+        }
+    }
+    return same;
 }
 
 module.exports = collect_same_elements;
