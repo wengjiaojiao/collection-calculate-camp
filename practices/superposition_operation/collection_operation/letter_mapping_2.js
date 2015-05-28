59dbe3fdef023a8415f7e79ca70c8f2ad88915ae
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 17:01:15 +0800

    ceil and map

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,9 +1,13 @@
 'use strict';
 
 function average_to_letter(collection) {
-
-  //在这里写入代码
+    var sum = 0;
+    for (var i = 0; i < collection.length; i++) {
+        sum += collection[i];
+        var aver = Math.ceil(sum / collection.length);
+    }
+    var letter = String.fromCharCode(aver+96);
+    return letter;
 }
 
 module.exports = average_to_letter;
-
