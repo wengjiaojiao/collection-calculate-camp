62dc9c15e83e20729e380ffcd67a07c72614afef
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 16:51:08 +0800

    map even to letter

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function even_to_letter(collection) {
+    var temp = [];
 
-  //在这里写入代码
+    for (var i = 0; i < collection.length; i++) {
+        if( collection[i] % 2 ===0) {
+            temp[temp.length] = collection[i];
+        }
+    }
+    var map = [];
+
+    for(var i = 0; i < temp.length; i++) {
+        var letter = String.fromCharCode(temp[i]+96);
+        map[map.length] = letter;
+    }
+    return map;
 }
 
 module.exports = even_to_letter;
