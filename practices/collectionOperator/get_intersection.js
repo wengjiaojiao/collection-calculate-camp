e6dfa2577ccd3d8730d6e24d44fa5efa21df7ce8
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 13:09:06 +0800

    aliging form

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,6 +2,7 @@
 
 function get_intersection(collection_a, collection_b) {
     var collection = [];
+    
     for(var i=0; i<collection_a.length; i++) {
         for(var j=0; j< collection_b.length; j++) {
             if(collection_a[i] === collection_b[j]) {
