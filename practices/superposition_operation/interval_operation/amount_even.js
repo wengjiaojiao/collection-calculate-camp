5a2d6bb6ce2aa69d73f99cc77014361ee81da0cf
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 17:17:05 +0800

    even and sum

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function amount_even(collection) {
+    var sum = 0;
 
-  //在这里写入代码
+    for (var i = 0; i < collection.length; i++) {
+        if(collection[i] % 2 === 0) {
+            sum += collection[i];
+        }
+    }
+    return sum;
 }
 
 module.exports = amount_even;
