c0f213a60b0a84a3f55921445657a04660cad1aa
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 15:52:06 +0800

    sort array from small to large

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,9 +1,17 @@
 'use strict';
 var rank_asc = function(collection){
-    var _ = require("../lodash/collection");
-    var result= _.sortBy(collection);
-    
-    return result.reverse();
+    var number ;
+
+    for (var i = 0; i < collection.length; i++) {
+        for (var j = i + 1; j < collection.length; j++) {
+            if(collection[i] < collection[j]) {
+                number = collection[i];
+                collection[i] = collection[j];
+                collection[j] = number;
+            }
+        }
+    }
+    return collection;
 };
 
 module.exports = rank_asc;
