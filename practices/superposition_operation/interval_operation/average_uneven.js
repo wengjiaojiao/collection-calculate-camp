ed8c71c3eacf168f649199b6594d5c8de62a9e81
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 17:26:29 +0800

    odd and aver

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function average_uneven(collection) {
+    var sum = 0;
+    var count = 0;
 
-  //在这里写入代码
+    for (var i = 0; i < collection.length; i++) {
+        if(collection[i] % 2 != 0) {
+            sum += collection[i];
+            count ++;
+        }
+    }
+    var aver = sum / count;
+    return aver;
 }
 
 module.exports = average_uneven;
