27dfe762e597c2a8107cd15feb721475fdddb8bd
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 15:55:34 +0800

    find the chain median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,35 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+    var string_array = [];
+    var num_array = [];
+
+    string_array = collection.split('->');
+    for (var i = 0; i < string_array.length; i++) {
+        num_array.push((parseInt(string_array[i])));
+    }
+    var temp;
+
+    for (var i = 0; i < num_array.length; i++) {
+        for (var j = 0; j < num_array.length; j++) {
+            if(num_array[i] > num_array[j]) {
+                temp = num_array[i];
+                num_array[i] = num_array[j];
+                num_array[j] = temp;	
+            }
+        }
+    }
+
+    var middle = num_array.length / 2;
+    var median;
+
+    if(num_array.length % 2 == 0 ) {
+        median = (num_array[middle] + num_array[middle-1]) / 2;
+    }else {
+        median = (num_array.length - 1) / 2;
+    }
+
+    return median;
 }
 
 module.exports = compute_chain_median;
