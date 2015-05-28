5cd43c535d8ebc0275a8aa8f47730aa02fe4eaa3
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 17:47:44 +0800

    map median to letter

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,29 @@
 'use strict';
 
 function median_to_letter(collection) {
+    var median;
+    var middle = parseInt(collection.length / 2);
 
-  //在这里写入代码
+    if(collection.length % 2 == 0 ) {
+        median = (collection[middle] + collection[middle-1]) / 2;
+    }else {
+        median = collection[middle];
+    }
+    median = Math.ceil(median);
+
+    var SIMPLE = 26;
+    var x = 0;
+    var letter ;
+
+    for (var i = 0; i < collection.length; i++) {
+        x = median/SIMPLE > 1 ? parseInt(median/SIMPLE) : x;
+        if(median > SIMPLE) {
+            median -= SIMPLE;
+            letter = String.fromCharCode(x+96);
+        }
+    }
+    letter += String.fromCharCode(median+96);
+    return letter;
 }
 
 module.exports = median_to_letter;
