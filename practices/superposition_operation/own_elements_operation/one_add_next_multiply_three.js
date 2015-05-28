e8733fd3776a55a67b28efa8ff03af7904021eef
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 18:31:29 +0800

    add and threetimes

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,11 @@
 'use strict';
 function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+    var temp = [];
+    for (var i = 0; i < collection.length; i++) {
+        if(i < collection.length -1) {
+            temp[i] = (collection[i] + collection[i+1]) * 3;
+        }
+    }
+    return temp;
 }
 module.exports = one_add_next_multiply_three;
