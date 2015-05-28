d439c9be4c8a23f73663e8eca32bd78a32e18898
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 20:26:16 +0800

    find the count appearse how much

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,12 +1,30 @@
 'use strict';
 
 function grouping_count(collection) {
+    var number = {};
 
-    var _ = require("../lodash/collection");
-
-    return _.countBy(collection,function(n) {
-        return Math.floor(n);
-    })
-}
+    for(var i = 0;i < collection.length;i++) {
+         if(!! number[collection[i]]) {
+             number[collection[i]] ++;
+         }else {
+             number[collection[i]] = 0;
+             number[collection[i]] ++;
+         }
+     }
+     return number;
+ }
 
 module.exports = grouping_count;
+
+
+
+
+
+//   number[collection[i]]=number[collection[i]]||0;
+//   number[collection[i]]++;
+
+
+/*return _.countBy(collection,function(n) {
+    return Math.floor(n);
+});
+*/
