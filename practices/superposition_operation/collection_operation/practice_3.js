0279f4a4f9e691ae808a5386b73d3579bcc39ff2
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-28 17:14:12 +0800

    odd *3+5 and calculate sum

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -1,9 +1,19 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
+    var temp = [];
 
-  //在这里写入代码
+    for (var i = 0; i < collection.length; i++) {
+        if(collection[i] % 2 != 0) {
+            temp[temp.length] = collection[i] * 3 + 5;
+        }
+    }
+    var sum = 0;
+
+    for (var j = 0; j < temp.length; j++) {
+        sum += temp[j];
+    }
+    return sum;
 }
 
 module.exports = hybrid_operation_to_uneven;
-
