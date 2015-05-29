292df879e47fea16a62755408655a6824f6ed80c
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-29 09:39:54 +0800

    serach the median of even elements

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,20 @@
 'use strict';
 var calculate_median = function(collection){
+    var median;
+    var num_array = [];
 
+    for (var i = 0; i < collection.length; i++) {
+        if(i % 2 == 0) {
+            num_array[num_array.length] = collection[i];
+        }
+    }
+    var middle = parseInt(num_array.length / 2);
+
+    if(num_array.length % 2 ==0) {
+        median = (num_array[middle] + num_array[middle - 1])/2;
+    }else {
+        median = num_array[middle];
+    }
+    return median;
 };
 module.exports = calculate_median;
