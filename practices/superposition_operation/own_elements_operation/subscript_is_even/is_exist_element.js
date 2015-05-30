290f4d36e44aab40ccf9cb8cdddb8208ecdeb6f2
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-30 16:41:29 +0800

    judge is exist

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,21 @@
 'use strict';
 var is_exist_element = function(collection,element){
+    var even_array = [];
 
-};
+    for (var i = 0; i < collection.length; i++) {
+        if(i % 2 == 0) {
+            even_array[even_array.length] = collection[i];
+        }
+    }
+    for (var j = 0; j < even_array.length; j++) {
+        if(even_array[j] == element) {
+            return true;
+        }
+    }
+    for (var j = 0; j < even_array.length; j++) {
+        if(even_array[j] != element) {
+            return false;
+        }
+    }
+}
 module.exports = is_exist_element;
