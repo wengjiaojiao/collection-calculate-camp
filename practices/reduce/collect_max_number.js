c19f0a22b7d711dea7e4b709b3ccd9a51cbaf4e1
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-30 17:51:46 +0800

    use my tool

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,14 +1,9 @@
 'use strict';
 
 function collect_max_number(collection) {
-    var max = 0;
+    var _ = require("../../mytool");
 
-    for (var i = 0; i < collection.length; i++) {
-        if(collection[max] < collection[i]) {
-            max = i;
-        }
-    }
-    return collection[max];
+    return _.max(collection);
 }
 
 module.exports = collect_max_number;
