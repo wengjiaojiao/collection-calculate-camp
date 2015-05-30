434c7e371cec2ccef14ac383305a845894923d92
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-30 18:28:17 +0800

    adjuest function

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,21 +1,17 @@
 'use strict';
 function map_to_even(collection){
-    var map = [];
-    for(var i = 0; i < collection.length; i++) {
-        collection[i] = collection[i] * 2;
-        map.push(collection[i]);
+    var result = [];
+    for (var i = 0; i < collection.length; i++) {
+        result.push(map(collection[i]));
     }
-    return map;
+    return result;
 }
+
+function map(i) {
+    return i * 2 ;
+}
+
+
+
 
 module.exports = map_to_even;
-
-
-
-
-/*return _.map(collection,twotimes);
-
-function twotimes(n) {
-return n * 2;
-}
-*/
