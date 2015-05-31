29b514ac399ef26ef2117c08b6a3770ade134edd
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-31 12:54:29 +0800

    use mylodash to judge element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,21 +1,15 @@
 'use strict';
 var is_exist_element = function(collection,element){
     var even_array = [];
+    var _ = require('../../../../mylodash.js');
 
     for (var i = 0; i < collection.length; i++) {
         if(i % 2 == 0) {
             even_array[even_array.length] = collection[i];
         }
     }
-    for (var j = 0; j < even_array.length; j++) {
-        if(even_array[j] == element) {
-            return true;
-        }
-    }
-    for (var j = 0; j < even_array.length; j++) {
-        if(even_array[j] != element) {
-            return false;
-        }
-    }
+    return _.exist(even_array,element);
+
 }
+
 module.exports = is_exist_element;
