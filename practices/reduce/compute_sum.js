daa5116c5b83509558bd0aa44c255fc60ea5d950
wengjiaojiao <wengjiaojiao@outlook.com>
2015-05-31 11:28:41 +0800

    use mylodash to calculate the sum

--- practices/reduce/compute_sum.js
+++ practices/reduce/compute_sum.js
@@ -1,11 +1,9 @@
 'use strict';
 
 function calculate_elements_sum(collection) {
-    var sum = 0;
-    for (var i = 0; i < collection.length; i++) {
-        sum += collection[i];
-    }
-    return sum;
+    var _ = require('../../mylodash.js');
+
+    return _.sum(collection);
 }
 
 module.exports = calculate_elements_sum;
