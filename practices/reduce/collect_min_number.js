908bef266c5c7cd53ff9be0c2d7e32bc3a388870
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-01 13:56:40 +0800

    use reduce to serach the min

--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -3,7 +3,10 @@
 function collect_min_number(collection) {
     var _ = require("../../mylodash.js");
 
-    return _.min(collection);
+    return _.reduce(collection,min);
 }
 
+function min(a,b) {
+    return Math.min(a,b);
+}
 module.exports = collect_min_number;
