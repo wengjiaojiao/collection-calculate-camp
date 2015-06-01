fac491ef80225f5bfd1acc08f5ab88fc1c8b7b71
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-01 13:55:28 +0800

    use reduce to serach the max

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -3,7 +3,10 @@
 function collect_max_number(collection) {
     var _ = require("../../mylodash.js");
 
-    return _.max(collection);
+    return _.reduce(collection,max);
 }
 
+function max(a,b) {
+    return Math.max(a,b);
+}
 module.exports = collect_max_number;
