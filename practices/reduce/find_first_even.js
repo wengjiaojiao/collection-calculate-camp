eb669db1e3549e41c176912e62b1612e5045eee6
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-01 15:50:18 +0800

    use reduce to call first

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,12 +2,17 @@
 function find_first_even(collection) {
     var _  =require('../../mylodash.js');
 
-    return _.first(collection,find);
+    var first = _.filter(collection,function(n){
+        if(n % 2 == 0) {
+            return n;
+        }
+    })
+
+    return _.reduce(first,function(a,b){
+        return a;
+    })
 }
 
-function find(n){
-    if( n % 2 == 0 ) {
-        return true ;
-    }
-}
+
+
 module.exports = find_first_even;
