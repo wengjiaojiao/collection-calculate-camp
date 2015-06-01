8027044902f30cf050e5a1c3f231ffdce9c2cb11
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-01 15:51:00 +0800

    calculate and sort

--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,5 +1,23 @@
 'use strict';
 var even_group_calculate_average = function(collection){
+    var even_array = [];
 
+    for (var i = 0; i < collection.length; i++) {
+        if(collection[i] % 2 == 0) {
+            even_array[even_array.length] = collection[i];
+        }
+    }
+    var one = [],two = [],three = [];
+
+    for (var j = 0; j < even_array.length; j++) {
+        if(even_array[j] / 10 < 1) {
+            one[one.length] = even_array[j];
+        }else if(even_array[j] / 10 >= 1 && even_array[j] / 10 <10) {
+            two[two.length] = even_array[j];
+        }else {
+            three[three.length] = even_array[j];
+        }
+    }
+    
 };
 module.exports = even_group_calculate_average;
