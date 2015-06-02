0a80a4bcfe0e92561ac946346c4002800e3c6134
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-02 19:43:32 +0800

    use mao

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,10 +1,11 @@
 'use strict';
 var map_to_three_multiples = function(collections){
-    var map = [];
-    for(var i = 0; i < collections.length; i++) {
-        collections[i] = collections[i] * 3;
-        map.push(collections[i]);
-    }
-    return map;
+    var _ = require("../../mylodash.js");
+
+    return _.map(collections,three);
+}
+
+function three(n) {
+    return n * 3;
 }
 module.exports = map_to_three_multiples;
