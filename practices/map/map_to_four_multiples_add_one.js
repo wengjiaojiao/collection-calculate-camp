119309615a1ab2c711c99849a701f2ca25aac322
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-02 19:41:44 +0800

    use map

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,10 +1,11 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-    var map = [];
-    for(var i = 0; i < collection.length; i++) {
-        collection[i] = collection[i] * 4 + 1;
-        map.push(collection[i]);
-    }
-    return map;
+    var _ = require("../../mylodash.js");
+
+    return _.map(collection,four);
 }
+
+function four(n) {
+    return n * 4 + 1;
+ }
 module.exports = map_to_four_multiples_add_one;
