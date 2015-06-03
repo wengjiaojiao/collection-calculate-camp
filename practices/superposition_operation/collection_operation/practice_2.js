5f377444b1af9e2140b59b8b9f4cf3a368d78e70
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 14:41:41 +0800

    use filter and map

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -1,14 +1,23 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-    var temp = [];
+    // var temp = [];
+    //
+    // for (var i = 0; i < collection.length; i++) {
+    //     if(collection[i] % 2 != 0) {
+    //         temp[temp.length] = collection[i] * 3 + 2;
+    //     }
+    // }
+    // return temp;
 
-    for (var i = 0; i < collection.length; i++) {
-        if(collection[i] % 2 != 0) {
-            temp[temp.length] = collection[i] * 3 + 2;
-        }
-    }
-    return temp;
+    var _ =require('../../../mylodash.js');
+
+    var temp = _.filter(collection,function(n) {
+        return n % 2 != 0;
+    });
+    return _.map(temp,function(n) {
+        return n * 3 + 2;
+    })
 }
 
 module.exports = hybrid_operation_to_uneven;
