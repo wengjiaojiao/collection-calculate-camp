8ff8c54df7669f7ac3567545faa940532dcec75d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 14:33:37 +0800

    use reduce

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,13 +1,16 @@
 'use strict';
 
 function average_to_letter(collection) {
-    var sum = 0;
-    for (var i = 0; i < collection.length; i++) {
-        sum += collection[i];
-        var aver = Math.ceil(sum / collection.length);
-    }
+    var _ =require("../../../mylodash.js");
+    var sum = _.reduce(collection,function(a,b){
+        return a + b;
+    });
+
+    var aver = Math.ceil(sum / collection.length);
     var letter = String.fromCharCode(aver+96);
+
     return letter;
 }
+
 
 module.exports = average_to_letter;
