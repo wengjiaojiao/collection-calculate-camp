4cf4ab5203f71f04aed94a1f1e03f3007c3b34f0
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 17:25:10 +0800

    use exist each

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,22 +1,31 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-    for(var i = 0; i < collection_a.length; i++) {
-        for(var j = 0; j < collection_b.length; j++) {
-            if(collection_a[i] === collection_b[j]) {
-                collection_b.splice(j,1);
-            }
+    var result = collection_a;
+
+    each(collection_b, function(n) {
+        if(exist(result, n)) {
+            result.push(n);
         }
+    });
+    return result;
+}
+
+function each(collection, fun) {
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i],i);
     }
-    for (var a = 0; a < collection_b.length; a++) {
-        collection_a[collection_a.length] = collection_b[a];
-    }
-    return collection_a;
+}
+
+function exist(collection, element) {
+    var result = true;
+
+    each(collection, function(n) {
+        if(n === element) {
+            result = false;
+        }
+    });
+    return result;
 }
 
 module.exports = get_union;
-
-
-
-
-//return _.union(collection_a,collection_b);
