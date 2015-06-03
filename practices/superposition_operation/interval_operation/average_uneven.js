fed899454149e22a4bce6755d9eb5da99b8bab82
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 15:46:16 +0800

    write reduce and filter

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,17 +1,62 @@
 'use strict';
 
 function average_uneven(collection) {
-    var sum = 0;
-    var count = 0;
+    // var sum = 0;
+    // var count = 0;
+    //
+    // for (var i = 0; i < collection.length; i++) {
+    //     if(collection[i] % 2 != 0) {
+    //         sum += collection[i];
+    //         count ++;
+    //     }
+    // }
+    // var aver = sum / count;
+    // return aver;
+    var odd = filter(collection, function(n) {
+        return n % 2 != 0;
+    });
 
-    for (var i = 0; i < collection.length; i++) {
-        if(collection[i] % 2 != 0) {
-            sum += collection[i];
-            count ++;
-        }
-    }
-    var aver = sum / count;
-    return aver;
+    var sum = reduce(odd, function(a, b) {
+        return a + b;
+    });
+    return sum / odd.length;
+
 }
+
+function each(collection, fun) {
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i],i);
+    }
+}
+
+function filter(collection, fun) {
+    var result = [];
+    each(collection, function(n) {
+        if(fun(n)) {
+            result.push(n);
+        }
+    });
+    return result;
+}
+
+function reduce(collection, fun) {
+    var result = [];
+
+    each(collection, function(n, i) {
+        if(i === 0) {
+            result = collection[0];
+        }else {
+            result = fun(result, n);
+        }
+    });
+    return result;
+}
+
+
+
+
+
+
+
 
 module.exports = average_uneven;
