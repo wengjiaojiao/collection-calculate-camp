988fcadd8889dde08c6ad5b4eee1c977790bd60d
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 15:33:24 +0800

    use foreach

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -1,12 +1,18 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-    for (var i = 0; i < collection.length; i++) {
-        if(collection[i] === element) {
-            return i;
+    var _ = require("../../mylodash.js");
+    var result = 0;
+    var exist = true;
+     _.foreach(collection,function(n,i){
+        if(n === element && exist) {
+            result = i;
+            exist =false;
         }
-    }
+    });
+    return result;
 }
+
 
 module.exports = calculate_elements_sum;
 
