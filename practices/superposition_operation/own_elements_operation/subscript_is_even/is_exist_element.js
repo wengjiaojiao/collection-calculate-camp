f9ffa69f3345b7d0e3f4c51da04d58b934b908b5
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 21:00:35 +0800

    use filter and exist

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,15 +1,13 @@
 'use strict';
-var is_exist_element = function(collection,element){
-    var even_array = [];
-    var _ = require('../../../../mylodash.js');
+var _ = require("../../../../mylodash.js");
 
-    for (var i = 0; i < collection.length; i++) {
-        if(i % 2 == 0) {
-            even_array[even_array.length] = collection[i];
-        }
-    }
-    return _.exist(even_array,element);
+var is_exist_element = function(collection, element){
+    var even = _.filter(collection,function(n,i) {
+        return i % 2 === 0;
+    });
 
+    return _.exist(even, element);
 }
+
 
 module.exports = is_exist_element;
