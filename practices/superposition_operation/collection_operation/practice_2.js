b8fdb77bc447e30f675ba22124df2ace998c990b
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 14:49:32 +0800

    rewrite filter map and reduce

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -1,23 +1,38 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-    // var temp = [];
-    //
-    // for (var i = 0; i < collection.length; i++) {
-    //     if(collection[i] % 2 != 0) {
-    //         temp[temp.length] = collection[i] * 3 + 2;
-    //     }
-    // }
-    // return temp;
 
-    var _ =require('../../../mylodash.js');
-
-    var temp = _.filter(collection,function(n) {
+    var temp = filter(collection,function(n) {
         return n % 2 != 0;
     });
-    return _.map(temp,function(n) {
+    return map(temp,function(n) {
         return n * 3 + 2;
     })
 }
 
+function each(collection,fun){
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i],i);
+    }
+}
+
+function filter(collection,fun) {
+    var result = [];
+
+    each(collection,function(n) {
+        if(fun(n)){
+            result.push(n);
+        }
+    });
+    return result;
+}
+
+function map(collection,fun) {
+    var result = [];
+
+    each(collection,function(n) {
+        result.push(fun(n));
+    });
+    return result;
+}
 module.exports = hybrid_operation_to_uneven;
