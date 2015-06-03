89edcc610c9f28db36d28414b0180dd73df00cd1
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 21:11:15 +0800

    use filter and reduce

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,17 +1,46 @@
 'use strict';
 var calculate_average = function(collection){
-    var even = [];
-
-    for (var i = 0; i < collection.length; i++) {
-        if((i+1) % 2 == 0) {
-            even[even.length] = collection[i];
-        }
-    }
-    var sum = 0;
-
-    for (var j = 0; j < even.length; j++) {
-        sum += even[j];
-    }
-    return sum/even.length;
+   var even = filter(collection, function(n, i) {
+       return  n % 2 ==0;
+   });
+   var sum = reduce(even, function(a, b) {
+       return a + b;
+   });
+   return sum / even.length;
 };
+
+
+function each(collection, fun) {
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i], i);
+    }
+}
+
+
+function filter(collection, fun) {
+    var result = [];
+
+    each(collection, function(n) {
+        if(fun(n)) {
+            result.push(n);
+        }
+    });
+    return result;
+}
+
+function reduce(collection, fun) {
+    var result = collection[0];
+
+    each(collection, function(n, i) {
+        if(i === 0) {
+            result = collection[0];
+        }else {
+            result = fun(result, n);
+        }
+    });
+    return result;
+}
+
+
+
 module.exports = calculate_average;
