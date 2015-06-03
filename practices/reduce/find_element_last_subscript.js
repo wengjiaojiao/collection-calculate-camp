682323203f31087c8584c6b70aad21dbe1d6723e
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-03 15:37:10 +0800

    use each to dispose

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -1,14 +1,20 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-    for (var i = collection.length-1; i >= 0; i--) {
-        if(collection[i] === element) {
-            return i;
+    var result = 0;
+    each(collection, function(n, i){
+        if(n === element) {
+            result = i;
         }
-    }
-
+    });
+    return result;
 }
 
+function each(collection, fun) {
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i], i);
+    }
+}
 module.exports = calculate_elements_sum;
 
 
