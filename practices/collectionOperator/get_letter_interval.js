0443fa9851395ececba83f4b89aedc2ff2d98a70
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-04 16:35:39 +0800

    use map and range

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,16 +2,42 @@
 
 function get_letter_interval(number_a, number_b) {
     var result = [];
-    
-    if(number_a <= number_b) {
-        for(var i=number_a; i<=number_b; i++) {
-            result.push(String.fromCharCode(i+96));
-        }
+
+    if(number_a != number_b) {
+        result = range(number_a, number_b);
     }else {
-        for(var j=number_a; j>=number_b; j--) {
-            result.push(String.fromCharCode(j+96));
-        }
+        result.push(number_a);
     }
+    return map(result, function(n){
+        return String.fromCharCode(n+96);
+    });
+}
+
+
+
+function range(a, b) {
+    var step = Math.abs(b - a) / (b - a);
+    var count = (b- a) / step;
+    var result = [];
+
+    for (var i = 0; i <= count; i++) {
+        var item = a + i * step;
+        result.push(item);
+    }
+    return result;
+}
+function each(collection, fun) {
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i], i);
+    }
+}
+
+function map(collection,fun) {
+    var result = [];
+
+    each(collection, function(n) {
+        result.push(fun(n));
+    });
     return result;
 }
 
