6d715852f86ace84f905df9593d02f6ace5ebc7e
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-04 16:43:15 +0800

    use exist

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -4,19 +4,32 @@ var _ =require("../../mylodash.js");
 function choose_common_elements(collection_a, collection_b) {
     var result = [];
 
-     _.filter(collection_a,function(n) {
-        _.foreach(collection_b,function(m) {
-            if(n === m) {
-                result.push(n);
-            }
-        })
-    })
+     each(collection_a, function(n) {
+         if(exist(collection_b, n)) {
+             result.push(n);
+         }
+     });
     return result;
 }
 
 module.exports = choose_common_elements;
 
+function each(collection, fun){
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i], i);
+    }
+}
 
+function exist(collection, element) {
+    var result = false;
+
+    each(collection, function(n) {
+        if(n === element) {
+            result = true;
+        }
+    });
+    return result;
+}
 
 
 
