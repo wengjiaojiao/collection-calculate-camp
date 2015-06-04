f9ebb8669a923f0c5a822d798b4d1a4388c86013
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-04 17:15:04 +0800

    use two each

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,14 +3,31 @@
 function choose_divisible_integer(collection_a, collection_b) {
     var result = [];
 
-    for(var i = 0; i < collection_a.length; i++) {
-        for(var j = 0; j < collection_b.length; j++) {
-            if(collection_a[i] % collection_b[j] == 0) {
-                result.push(collection_a[i]);
+    each(collection_a,function (num_a) {
+        each(collection_b,function (num_b) {
+            if(num_a % num_b === 0){
+                result.push(num_a);
             }
-        }
-    }
+        })
+    });
     return result;
 }
 
 module.exports = choose_divisible_integer;
+
+function each(collection, fun) {
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i], i);
+    }
+}
+
+function filter(collection, fun) {
+    var result = [];
+
+    each(collection, function(n) {
+        if(fun(n)) {
+            result.push(n);
+        }
+    });
+    return result;
+}
