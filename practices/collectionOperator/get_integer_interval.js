bf132a419b08153048b598715b47574f6360c674
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-04 16:06:04 +0800

    use range

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,15 +2,23 @@
 
 function get_integer_interval(number_a, number_b) {
     var result = [];
-
-    if(number_a <= number_b) {
-        for(var i=number_a; i<=number_b; i++) {
-            result.push(i);
-        }
+    
+    if(number_a != number_b) {
+        result = range(number_a, number_b);
     }else {
-        for(var j=number_a; j>=number_b; j--) {
-            result.push(j);
-        }
+        result.push(number_a);
+    }
+    return result;
+}
+
+function range (a, b) {
+    var step = Math.abs(b - a) / (b - a);
+    var count = (b - a) / step;
+    var result = [];
+
+    for (var i = 0; i <= count; i++) {
+        var item = a + i * step;
+        result.push(item);
     }
     return result;
 }
