fc68b667ea58ab2050b24efafe08c5fb611e7b52
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-04 16:16:07 +0800

    use range and filter

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,35 +1,52 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-    var collection = [];
+    var result = [];
 
-    if(number_a <= number_b) {
-        for(var i=number_a; i<=number_b; i++) {
-            collection.push(i);
-        }
+    if(number_a != number_b) {
+        result = range(number_a, number_b);
     }else {
-        for(var j=number_a; j>=number_b; j--) {
-            collection.push(j);
-        }
-    }
-    var even = [];
-
-    for(var k=0; k<collection.length; k++) {
-        if(collection[k] % 2 == 0) {
-            even.push(collection[k]);
-        }
+        result.push(number_a);
     }
 
-    return even;
+    return filter(result,function(n, i) {
+        return n % 2 === 0;
+    });
+}
+
+
+
+function range(a, b) {
+    var step = Math.abs(b - a) / (b - a);
+    var count = (b - a) / step;
+    var result = [];
+
+    for (var i = 0; i <= count; i++) {
+        var item = a + i * step;
+        result.push(item);
+    }
+    return result;
+}
+
+
+
+function each (collection, fun) {
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i], i);
+    }
+}
+
+
+
+function filter(collection,fun) {
+    var result = [];
+
+    each(collection, function(n, i) {
+        if(fun(n, i)) {
+            result.push(n);
+        }
+    });
+    return result;
 }
 
 module.exports = get_integer_interval_2;
-
-
-
-
-
-//var result = _.remove(collection,function(n){
-//    return n % 2 == 0;
-//    }
-//)
