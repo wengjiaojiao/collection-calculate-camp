297f592ca8a7136c0962589675aee9f5eb2ffda9
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-05 14:03:47 +0800

    update and use filter

--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,23 +1,47 @@
-'use strict';
-var even_group_calculate_average = function(collection){
-    var even_array = [];
+var even_group_calculate_average = function(collection) {
+    var temp = {};
+    var result = [];
 
-    for (var i = 0; i < collection.length; i++) {
-        if(collection[i] % 2 == 0) {
-            even_array[even_array.length] = collection[i];
+    var even  = filter(collection,function(n,i){
+        return (i+1) % 2 === 0 && n % 2 === 0;
+    });
+
+    each(even,function(n,i){
+        var collection_len = n.toString().length;
+
+        temp[collection_len] = temp[collection_len] || [];
+        temp[collection_len].push(n);
+    })
+
+    for(key in temp) {
+        var sum = 0;
+        var key_length = temp[key].length;
+
+        if(key_length != 0) {
+            for (var i = 0; i < key_length; i++) {
+                sum += temp[key][i];
+            }
+            result.push(sum / key_length);
         }
     }
-    var one = [],two = [],three = [];
+    return result.length === 0 ? [0] : result;
+}
 
-    for (var j = 0; j < even_array.length; j++) {
-        if(even_array[j] / 10 < 1) {
-            one[one.length] = even_array[j];
-        }else if(even_array[j] / 10 >= 1 && even_array[j] / 10 <10) {
-            two[two.length] = even_array[j];
-        }else {
-            three[three.length] = even_array[j];
-        }
-    }
-    
-};
 module.exports = even_group_calculate_average;
+
+filter = function(collection,fun) {
+    var result = [];
+
+    each(collection,function(n,i){
+        if(fun(n,i)) {
+            result.push(n);
+        }
+    });
+    return result;
+}
+
+each = function(collection,fun){
+    for (var i = 0; i < collection.length; i++) {
+        fun(collection[i],i);
+    }
+}
