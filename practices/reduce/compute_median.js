ef69b1952618ef71c77232d17ad1e2a14b8def52
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-05 13:12:27 +0800

    use rank and median

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -3,26 +3,37 @@
 function compute_median(collection) {
     var number;
 
+    rank(collection, function(a, b){
+        return a > b;
+    });
+    return median(collection);
+}
+
+module.exports = compute_median;
+
+function rank(collection,fun) {
+    var number;
+
     for (var i = 0; i < collection.length; i++) {
         for (var j = i + 1; j < collection.length; j++) {
-            if(collection[i] > collection[j]) {
+            if(fun(collection[i], collection[j])) {
                 number = collection[i];
                 collection[i] = collection[j];
                 collection[j] = number;
             }
         }
     }
+    return collection;
+}
 
+function median(collection) {
     var median;
     var middle = parseInt(collection.length / 2);
-    
-    if(collection.length % 2 == 0 ) {
-        median = (collection[middle] + collection[middle-1]) / 2;
+
+    if(collection.length % 2 ===0) {
+        median = (collection[middle] + collection[middle - 1 ]) / 2;
     }else {
         median = collection[middle];
     }
-
     return median;
 }
-
-module.exports = compute_median;
