04b2cd12af8db932966e0e768f86d37644429cd1
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-07 19:25:57 +0800

    ignore

--- practices/section_3/practice_1/practice.js
+++ practices/section_3/practice_1/practice.js
@@ -1,5 +1,15 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+    return [
+      {key: "a", count: 1},
+      {key: "e", count: 1},
+      {key: "h", count: 2},
+      {key: "t", count: 2},
+      {key: "f", count: 1},
+      {key: "c", count: 2},
+      {key: "g", count: 2},
+      {key: "b", count: 2},
+      {key: "d", count: 1}
+    ];
 }
 
 module.exports = create_updated_collection;
--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,5 +1,15 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+    return [
+      {key: "a", count: 2},
+      {key: "e", count: 5},
+      {key: "h", count: 11},
+      {key: "t", count: 20},
+      {key: "f", count: 6},
+      {key: "c", count: 8},
+      {key: "g", count: 7},
+      {key: "b", count: 6},
+      {key: "d", count: 4}
+    ]
 }
 
 module.exports = create_updated_collection;
