887015c60fffc5995c59146d9fca038017062e94
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-07 18:41:19 +0800

    use toString

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,16 +1,18 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-    var result = true;
+//     var result = true;
+//
+//     if(collection_a.length === collection_b.length) {
+//         each(collection_a, function(n, i) {
+//             if(n != collection_b[i]) {
+//                 result = false;
+//             }
+//         });
+//     }
+//     return result;
 
-    if(collection_a.length === collection_b.length) {
-        each(collection_a, function(n, i) {
-            if(n != collection_b[i]) {
-                result = false;
-            }
-        });
-    }
-    return result;
+    return collection_a.toString() === collection_b.toString();
 }
 module.exports = compare_collections;
 
