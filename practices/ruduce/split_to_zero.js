4bd9aa35b46007917206ccfc1e4f89b25521fc9b
wengjiaojiao <wengjiaojiao@outlook.com>
2015-06-07 19:10:57 +0800

    spilt to zero

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,20 @@
 'use strict';
 
-function collect_max_number(number) {
-  //在这里写入代码
+function spilt_to_zero(number, interval) {
+    var result = [];
+    result.push(number);
+    var count = number / interval;
+
+    if(number % interval != 0) {
+        count = parseInt(count) + 1;
+    }
+
+    for (var i = 1;i <= count; i++) {
+        var number_s = (number - interval).toFixed(1);
+        var number = Number(number_s);
+        result.push(number);
+    }
+    return result;
 }
 
-module.exports = collect_max_number;
+module.exports = spilt_to_zero;
